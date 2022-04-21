b6f6cc034c4b6bcf0f9a33790471f59fade96b4c
Riyaj Hingani <riyajhingani238@gmail.com>
2022-04-21 11:13:55 +0530

    Add project files.

---
+++ phase 1 project/TeachersData/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachersData
+{
+    internal class Teacher
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public char Section { get; set; }
+    }
+}
