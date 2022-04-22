99db33fc304978b98e3a75c13180828b5bb0474f
Riyaj Hingani <riyajhingani238@gmail.com>
2022-04-22 09:10:44 +0530

    retrieve student data with sorting and searching

---
+++ student data/SortedStudentData/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace SortedStudentData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0;
+            Console.WriteLine(" Sorted Student data present in the file is\n");
+            string file = @"D:\studentdata.txt";
+            string[] details = File.ReadAllLines(file);
+            Array.Sort(details);
+            foreach (string ln in details)
+            {
+                Console.WriteLine(ln);
+            }
+            Console.WriteLine("\nEnter Name of student you want to Search");
+            string Name = Console.ReadLine();
+            
+            for (int i = 0; i < details.Length; i++)
+            {
+                
+               string[] a= details[i].Split(',');
+                if(Name==a[0])
+                {
+                    Console.WriteLine($"Student data found at line {i+1} ");
+                    m = 1;
+                    break;
+                }
+               
+            }
+            if (m == 0)
+            Console.WriteLine("student data not found");
+         
+            Console.ReadLine();
+        }
+    }
+}
---
+++ student data/StudentData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StudentData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student data present in the file is\n");
+            string file = @"D:\studentdata.txt";
+            string[] details = File.ReadAllLines(file);
+            foreach (string ln in details)
+            {
+                Console.WriteLine(ln);
+            }
+            Console.ReadLine();
+        }
+    }
+}
